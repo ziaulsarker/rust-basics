eacbfc6c6d746e91557dc05914308b5c9b5f1bd9
zsarker <ziaul.sarker@aexp.com>
2020-11-23 23:19:07 -0500

    complete guessing gaame

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,31 @@ fn main() {
     // createa a new random number from rand library
     let _secret_number = rand::thread_rng().gen_range(0, 101);
 
-    println!("please input your number");
-    // create new instance of a string that is mutable;
-    let mut guess = String::new();
+    loop{
 
-    // use the standard io libarary to read the input pro the user and save it in the staing "guess" which returns RESULT with a expect property to be called if the input could not be read;
-    io::stdin().read_line(&mut guess)
-        .expect("failed to read line");
+        println!("please input your number");
+        // create new instance of a string that is mutable;
+        let mut guess = String::new();
 
-    let guess : i32 = guess.trim().parse()
-        .expect("failed to parse, please use a valid number");
+        // use the standard io libarary to read the input pro the user and save it in the staing "guess" which returns RESULT with a expect property to be called if the input could not be read;
+        io::stdin().read_line(&mut guess)
+            .expect("failed to read line");
 
-    // prints the input val that is saved in guess variable;
-    println!("you guessed {}", guess);
+        let guess : i32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
 
-
-    match guess.cmp(&_secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Equal => println!("Wiiner winner chicken dinner"),
-        Ordering::Greater => println!("Too Big"),
+        // prints the input val that is saved in guess variable;
+        println!("you guessed {}", guess);
+    
+        match guess.cmp(&_secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Equal => {
+                println!("Wiiner winner chicken dinner");
+                break;
+            },
+            Ordering::Greater => println!("Too Big"),
+        }
     }
 }
